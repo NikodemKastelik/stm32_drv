e6e1206412cb9175be5c3f72f06f1ba6a57ccd70
Nikodem Kastelik <nikodemkastelik@gmail.com>
2020-01-28 22:27:34 +0100

    Implement DRV_CHECK macro for checking config symbols #36

--- drv/inc/drv_common.h
+++ drv/inc/drv_common.h
@@ -3,4 +3,18 @@
 
 #include <stm32drv_config.h>
 
+/**
+ * @brief Macro for checking if given symbol is defined and set to '1'.
+ *
+ * @param[in] cfg Symbol to check.
+ *
+ * @retval '1' If given symbol is defined and set to '1'.
+ * @retval '0' If given symbol is undefined or set to different value than '1'.
+ */
+#define DRV_CHECK(cfg)   _DRV_CHECK(cfg)
+#define _DUMMY_ARG_1 0,
+#define _DRV_CHECK(cfg)  __DRV_CHECK(_DUMMY_ARG_ ## cfg)
+#define __DRV_CHECK(cfg) ___DRV_CHECK(cfg 1, 0)
+#define ___DRV_CHECK(_ignored, result, ...) result
+
 #endif // _DRV_COMMON_H_
