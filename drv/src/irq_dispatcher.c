32f771eb42f3273e75857b02f71d85945920640e
Nikodem Kastelik <nikodemkastelik@gmail.com>
2020-02-29 18:43:16 +0100

    Add EXTI driver IRQ handler to IRQ dispatcher #38

--- drv/src/irq_dispatcher.c
+++ drv/src/irq_dispatcher.c
@@ -1,5 +1,6 @@
 #include <drv_common.h>
 #include <drv_usart.h>
+#include <drv_exti.h>
 
 #if DRV_CHECK(DRV_CONFIG_USART1_ENABLED)
 void USART1_IRQHandler(void)
@@ -12,5 +13,22 @@ void USART1_IRQHandler(void)
 void USART2_IRQHandler(void)
 {
     drv_usart_irq_handler(DRV_USART_INST2_IDX);
+}
+#endif
+
+#if DRV_CHECK(DRV_CONFIG_EXTI_ENABLED)
+void EXTI0_1_IRQHandler(void)
+{
+    drv_exti_irq_handler(hal_exti_pending_get(EXTI) & 0x3ul);
+}
+
+void EXTI2_3_IRQHandler(void)
+{
+    drv_exti_irq_handler(hal_exti_pending_get(EXTI) & 0xCuL);
+}
+
+void EXTI4_15_IRQHandler(void)
+{
+    drv_exti_irq_handler(hal_exti_pending_get(EXTI) & 0xFFF0uL);
 }
 #endif
